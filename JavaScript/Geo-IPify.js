9d2deaed179f5875caf54822eeff53035711472d
Amber Biggart <amberbiggart55@gmail.com>
2022-08-30 14:08:43 -0600

    WIP on getting map to display

--- JavaScript/Geo-IPify.js
+++ JavaScript/Geo-IPify.js
@@ -1,13 +1,79 @@
-<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
-{/* <script type="text/javascript"> */}
-    var ip = "8.8.8.8";
-    var api_key = "your_api_key";
-    $(function () {
-       $.ajax({
-           url: "https://geo.ipify.org/api/v1",
-           data: {apiKey: api_key, ipAddress: ip},
-           success: function(data) {
-               $("body").append("<pre>"+ JSON.stringify(data,"",2)+"</pre>");
-           }
-       });
-    });
+
+const getIpAddress = async () => {
+    let ip = document.getElementById("ip").value;
+    let request = `https://geo.ipify.org/api/v2/country,city?apiKey=at_lyEpJjRAahG87s8BMrWadFehAGIyg&ipAddress=${ip || '8.8.8.8'}`;
+    
+    let response = await fetch(request);
+    if (!response.ok) {
+        throw Error(`There is an error with status ${response.status}`);
+    }
+    let ipAddress = await response.json();
+    console.log(ipAddress);
+    return ipAddress;
+};
+
+const displayIpAddress = async () => {
+    const addressElement = document.getElementById("address");
+    const locationElement = document.getElementById("location");
+    const timezoneElement = document.getElementById("timezone");
+    const ispElement = document.getElementById("isp");
+    
+    let ipResult = await getIpAddress();
+    addressElement.innerText = ipResult.ip;
+    console.log(`ip: ${ipResult.ip}`);
+    let region = ipResult.location.region;
+    console.log(`region: ${region}`);
+    let country = ipResult.location.country;
+    console.log(`country: ${country}`);
+    locationElement.innerText = region + ", " + country;
+    timezoneElement.innerText = ipResult.location.timezone;
+    ispElement.innerText = ipResult.isp;
+
+    // for map
+    const lat = ipResult.location.lat;
+    const lng = ipResult.location.lng;
+    console.log(`lat: ${lat} and lng: ${lng}`);
+    return [lat, lng];
+}
+
+let formBtn = document.querySelector(".submit-btn");
+formBtn.addEventListener("click", displayIpAddress);
+
+const form = document.querySelector("form");
+form.addEventListener("keypress", function (e) {
+ e.preventDefault();
+  if (e.key === "Enter") {
+    displayIpAddress();
+  }
+});
+
+
+const displayMap = () => {
+    const displayAddress = displayIpAddress();
+    console.log(displayAddress);
+//    displayIpAddress();
+
+    const map = L.map('map').setView(displayAddress || [51.505, -0.09], 13);
+    // const map = L.map('map').setView([51.505, -0.09], 13);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+    
+    L.marker(displayAddress).addTo(map)
+        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+        .openPopup();
+
+}
+
+// const map = L.map('map').setView([51.505, -0.09], 13);
+
+// L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+//     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+// }).addTo(map);
+
+// L.marker([51.505, -0.09]).addTo(map)
+//     .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+//     .openPopup();
+
+displayMap();
